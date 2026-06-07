86193f557a8e3ab159e8f5f394b49099ebdc5e91
Jan T. Sott <git@idleberg.com>
2026-06-07 14:32:36 +0200

    fix: update base path

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://nsis-dev.github.io',
-	base: '/highlight.js-nsis/',
+	base: '/highlight.js/',
 	outDir: 'docs',
 	output: 'static',
 	vite: {
