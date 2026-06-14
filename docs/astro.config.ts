3b83b23feb3804adcd1def5dd70bd02dc561d8a6
Jan T. Sott <git@idleberg.com>
2026-06-14 21:42:47 +0200

    chore: move demo page

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://idleberg.github.io',
-	base: process.env.CI ? '/nsis-javascript-tools/highlightjs/' : '/',
-	outDir: 'docs',
-	output: 'static',
+	base: process.env.CI ? '/nsis-javascript-tools/' : '/',
+	outDir: 'dist',
 	vite: {
 		plugins: [tailwindcss()],
 	},
