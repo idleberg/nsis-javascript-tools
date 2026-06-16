5a427f4b5fb155150b0be4e45a7b9b60e43242ad
Jan T. Sott <git@idleberg.com>
2026-06-16 22:09:53 +0200

    chore: update tooling

--- packages/prismjs/src/nsis.js
+++ packages/prismjs/src/nsis.js
@@ -1,27 +1,27 @@
 /*! prism-nsis | MIT License | github.com/idleberg/prismjs-nsis */
 
 Prism.languages.nsis = {
-	'comment': {
+	comment: {
 		pattern: /(^|[^\\])(\/\*[\s\S]*?\*\/|[#;].*)/,
 		lookbehind: true,
-		greedy: true
+		greedy: true,
 	},
-	'string': {
+	string: {
 		pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
-		greedy: true
+		greedy: true,
 	},
-	'keyword': {
+	keyword: {
 		pattern: /(^\s*)%NSIS_KEYWORDS%\b/m,
-		lookbehind: true
+		lookbehind: true,
 	},
-	'property': /\b%NSIS_PROPERTIES%\b/,
-	'constant': /\${[\w.:^-]+}|\$\([\w.:^-]+\)/i,
-	'variable': /\$\w+/i,
-	'number': /\b-?(?:0x[\dA-Fa-f]+|\d*\.?\d+(?:[Ee]-?\d+)?)\b/,
-	'operator': /--?|\+\+?|<=?|>=?|==?=?|&&?|\|\|?|[?*/~^%]/,
-	'punctuation': /[{}[\];(),.:]/,
-	'important': {
-		pattern: /(^\s*)%NSIS_IMPORTANT%\b/mi,
-		lookbehind: true
-	}
+	property: /\b%NSIS_PROPERTIES%\b/,
+	constant: /\${[\w.:^-]+}|\$\([\w.:^-]+\)/i,
+	variable: /\$\w+/i,
+	number: /\b-?(?:0x[\dA-Fa-f]+|\d*\.?\d+(?:[Ee]-?\d+)?)\b/,
+	operator: /--?|\+\+?|<=?|>=?|==?=?|&&?|\|\|?|[?*/~^%]/,
+	punctuation: /[{}[\];(),.:]/,
+	important: {
+		pattern: /(^\s*)%NSIS_IMPORTANT%\b/im,
+		lookbehind: true,
+	},
 };
